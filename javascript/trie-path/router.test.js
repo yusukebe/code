f0c1b5e1ac06913a58465f77caab1d54889a85eb
Yusuke Wada <yusuke@kamawada.com>
2021-12-17 15:01:02 +0900

    Add complex pattern

--- javascript/trie-path/router.test.js
+++ javascript/trie-path/router.test.js
@@ -1,3 +1,4 @@
+const { expect } = require('@jest/globals')
 const Router = require('./router')
 const router = Router()
 
@@ -15,6 +16,37 @@ describe('Basic Usage', () => {
     expect(res.handler).toBe('post root')
 
     res = router.matchRoute('PUT', '/')
+    expect(res).toBeNull()
+
+    res = router.matchRoute('GET', '/nothing')
+    expect(res).toBeNull()
+  })
+})
+
+describe('Complex', () => {
+  it('Named Param', () => {
+    router.get('/entry/:id', 'get entry')
+    res = router.matchRoute('GET', '/entry/123')
+    expect(res).not.toBeNull()
+    expect(res.handler).toBe('get entry')
+    expect(res.params['id']).toBe('123')
+  })
+  it('Wildcard', () => {
+    router.get('/wild/*/card', 'get wildcard')
+    res = router.matchRoute('GET', '/wild/xxx/card')
+    expect(res).not.toBeNull()
+    expect(res.handler).toBe('get wildcard')
+  })
+  it('Regexp', () => {
+    router.get('/post/:date{[0-9]+}/:title{[a-z]+}', 'get post')
+    res = router.matchRoute('GET', '/post/20210101/hello')
+    expect(res).not.toBeNull()
+    expect(res.handler).toBe('get post')
+    expect(res.params['date']).toBe('20210101')
+    expect(res.params['title']).toBe('hello')
+    res = router.matchRoute('GET', '/post/onetwothree')
+    expect(res).toBeNull()
+    res = router.matchRoute('GET', '/post/123/123')
     expect(res).toBeNull()
   })
 })
